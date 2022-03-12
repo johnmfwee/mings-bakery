d98aa2a75ae09f6de880858d38b8939cf28be0a0
John Wee <johnmfwee@gmail.com>
2022-03-13 07:48:05 +1100

    feat(prettier): prettier config

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+  arrowParens: 'always',
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  enfOfLine: 'lf',
+  semi: false,
+  tabWidth: 2,
+}
+
+module.exports = options
